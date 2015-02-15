5e60b3c040f28a698da756d496f0828796cdb6b4
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2015-02-15 20:03:58 +0800

    add missing import

--- homework_3/src/client.c
+++ homework_3/src/client.c
@@ -2,6 +2,10 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
 #include "common.h"
 
 void print_usage() {
